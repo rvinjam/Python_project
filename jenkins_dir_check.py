10c740ca29d588613be9a7c85b4b19a5a349b103
ramarao vinjam <ramarao.vinjam@gmail.com>
2023-11-08 13:01:20 +0530

    added all py files

---
+++ jenkins_dir_check.py
@@ -0,0 +1,37 @@
+import os
+import os_check
+import validate_jenkins_dir
+import sys
+from xml.dom import ValidationErr
+
+def get_jenkins_home_directory():
+  """Gets the Jenkins home directory.   Returns:     The Jenkins home directory.   """
+
+  jenkins_home_directory = os.environ.get("JENKINS_HOME")
+  if jenkins_home_directory is None:
+    os_name = os_check.get_platform()
+    if os_name == "Windows":
+     jenkins_home_directory = "C:\ProgramData\Jenkins\.jenkins"
+     print("The Jenkins home directory is not set and using the default directory i.e :", jenkins_home_directory)
+     exit(1)
+    else :
+     jenkins_home_directory = "/var/lib/jenkins"
+     print("The Jenkins home directory is not set and using the default directory i.e :", jenkins_home_directory)
+     exit(1)
+    
+  return jenkins_home_directory
+
+def main():
+  """The main function."""
+  jenkins_home_directory = get_jenkins_home_directory()
+  print("The Jenkins home directory is:", jenkins_home_directory)
+  try:
+    validate_jenkins_dir.validate_jenkins_directory_structure(jenkins_home_directory)
+  except ValidationErr as e:
+    print(e)
+    sys.exit(1)
+  
+
+
+if __name__ == "__main__":
+  main()
