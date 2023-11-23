b06932917f29d3dbfe1e12c1d5138af69f29c39f
ramarao vinjam <ramarao.vinjam@gmail.com>
2023-11-23 14:12:38 +0530

    Job naming convestion

--- job_naming_convension.py
+++ job_naming_convension.py
@@ -1,4 +1,5 @@
 import re
+import requests
 
 def check_job_name(job_name):
   """
@@ -28,13 +29,29 @@ def check_job_name(job_name):
     return False
 
   return True
+# Replace these variables with your Jenkins server information
+jenkins_url = 'http://localhost:8080/'
+username = 'admin'
+api_token = '111266e5a6cc3260aa208f6355256bec27'  # You can generate this token in Jenkins
+# Make an API request to get job information
+response = requests.get(f'{jenkins_url}/api/json', auth=(username, api_token))
+if response.status_code == 200:
+   data = response.json()
+   jobs = data['jobs']
+   job_name = ""
+   if jobs:
+       print("List of Jenkins Jobs:")
+       for job in jobs:
+           print(job['name'])
+          # Get the job name from the user.
+           job_name = job['name']
 
-if __name__ == '__main__':
-  # Get the job name from the user.
-  job_name = input('Enter the job name: ')
-
-  # Check if the job name follows the convention.
-  if check_job_name(job_name):
-    print('The job name follows the convention.')
-  else:
-    print('The job name does not follow the convention.')
+          # Check if the job name follows the convention.
+           if check_job_name(job_name):
+            print('The job name ', job_name ,' follows the convention.')
+           else:
+            print('The job name ', job_name ,' does not follows the convention.')
+   else:
+        print("No jobs found in Jenkins.")
+else:
+    print(f"Failed to retrieve job information. Status code: {response.status_code}")   
