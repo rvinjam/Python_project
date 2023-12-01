ce152372ece9dfe219c618f352d6f7b3d9cc3893
ramarao vinjam <ramarao.vinjam@gmail.com>
2023-12-01 17:05:40 +0530

    removed extra py file

--- validate_jenkins_dir_structure.py
+++ validate_jenkins_dir_structure.py
@@ -1,6 +1,5 @@
 import os
 import os_check
-import check_dir_structure
 import sys
 from xml.dom import ValidationErr
 
@@ -20,13 +19,40 @@ def get_jenkins_home_directory():
      exit(1)
     
   return jenkins_home_directory
+def validate_jenkins_directory_structure(jenkins_directory):
+  """Validates the Jenkins directory structure.
+
+  Args:
+    jenkins_directory: The path to the Jenkins directory.
+
+  Raises:
+    ValidationErr: If the Jenkins directory structure is invalid.
+  """
+
+  if not os.path.exists(jenkins_directory):
+    raise ValidationErr("Jenkins directory does not exist.")
+
+  if not os.path.isdir(jenkins_directory):
+    raise ValidationErr("Jenkins directory is not a directory.")
+
+  jenkins_files = [
+    "jenkins.yaml",
+    "jobs",
+    "plugins",
+    "secrets",
+  ]
+
+  for jenkins_file in jenkins_files:
+    if not os.path.exists(os.path.join(jenkins_directory, jenkins_file)):
+      raise ValidationErr("Missing Jenkins file {}.".format(jenkins_file))
+
 
 def main():
   """The main function."""
   jenkins_home_directory = get_jenkins_home_directory()
   print("The Jenkins home directory is:", jenkins_home_directory)
   try:
-    check_dir_structure.validate_jenkins_directory_structure(jenkins_home_directory)
+    validate_jenkins_directory_structure(jenkins_home_directory)
   except ValidationErr as e:
     print(e)
     sys.exit(1)
