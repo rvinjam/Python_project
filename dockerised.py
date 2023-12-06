205c6742570c77c78057822d09728de39e81c121
ramarao vinjam <ramarao.vinjam@gmail.com>
2023-12-06 14:05:18 +0530

    updated the py  jenkin dcokerised or not

---
+++ dockerised.py
@@ -0,0 +1,9 @@
+import os 
+def is_running_in_docker():
+     # Check if the '/.dockerenv' file 
+     # exists, which is typically present 
+     # in a Docker container 
+     return os.path.exists('/.dockerenv') 
+if is_running_in_docker(): 
+    print("Jenkins is running in a Docker container.") 
+else: print("Jenkins is not running in a Docker container.")
--- is_jenkins_dockerised.py
+++ is_jenkins_dockerised.py
@@ -1,15 +1,22 @@
-import subprocess
-
-def is_jenkins_dockerised():
-  """Returns True if Jenkins is dockerised, False otherwise."""
-  output = subprocess.check_output(["docker", "ps"])
-  
-  
-  print(output)
-  return 'jenkins' in output
-
-if __name__ == '__main__':
-  if is_jenkins_dockerised():
-    print('Jenkins is dockerised')
-  else:
-    print('Jenkins is not dockerised')
+import requests
+def is_jenkins_dockerized(jenkins_url):
+   try:
+       # Make an HTTP request to Jenkins
+       response = requests.get(jenkins_url)
+       # Check if the 'Server' header contains 'Docker'
+       server_header = response.headers.get('Server', '')
+       if 'Docker' in server_header:
+           return True
+       # Check if the response content contains Docker-related information
+       if 'docker' in response.text.lower():
+           return True
+       return False
+   except Exception as e:
+       print(f"Error: {e}")
+       return False
+# Replace this with your Jenkins server URL
+jenkins_url = "http://localhost:8080"
+if is_jenkins_dockerized(jenkins_url):
+   print("Jenkins is running in a Docker container.")
+else:
+   print("Jenkins is not running in a Docker container.")
