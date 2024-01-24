84f0e7e94926c0564cd7693d1012ef4a03cf1b5a
ramarao vinjam <ramarao.vinjam@gmail.com>
2024-01-24 10:47:54 +0530

    update

--- integration/git_connect.py
+++ integration/git_connect.py
@@ -1,10 +1,9 @@
 from github import Github
-# Replace 'your_access_token' with your GitHub access token
-access_token = 'your_access_token'
+access_token = ''
 # Create a GitHub instance using the access token
 github_instance = Github(access_token)
 # Specify the repository details
-repo_owner = 'owner_username'
-repo_name = 'repository_name'
+repo_owner = ''
+repo_name = ''
 # Get the repository
 repository = github_instance.get_repo(f'{repo_owner}/{repo_name}')
